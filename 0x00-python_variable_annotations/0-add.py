e665963542c0061ac114a9e4b04cc47b0a9d29f7
escaper <assaoui54@gmail.com>
2024-04-19 02:09:25 +0000

    added add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Documentation """
+
+
+def add(a: float, b: float) -> float:
+    """
+    This function takes two floats as input and returns their sum.
+
+    Parameters:
+    a (int): The first integer.
+    b (int): The second integer.
+
+    Returns:
+    int: The sum of the two floats.
+    """
+    return a + b
